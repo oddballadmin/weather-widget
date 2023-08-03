36c2f9bf3ca105dfed845ceeccd49971c191f8af
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2023-08-02 22:20:36 -0400

    Added manifest.json

---
+++ scripts/widget.js
@@ -0,0 +1,26 @@
+// Replace YOUR_API_KEY with your WeatherAPI key
+const API_KEY = "d3ad3f5975fe4e2a85112129232307";
+const locationDiv = document.getElementById("location");
+const temperatureDiv = document.getElementById("temperature");
+
+// Get user's location
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        // Get weather data from WeatherAPI
+        fetch(
+            `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${position.coords.latitude},${position.coords.longitude}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                // Update widget with location and temperature
+                locationDiv.innerText = `Location: ${data.location.name}`;
+                temperatureDiv.innerText = `Temperature: ${data.current.temp_f}°F`;
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    },
+    (error) => {
+        console.warn("Error:", error.message);
+    }
+);
